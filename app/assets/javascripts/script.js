ce8d7aef9a5cc785489c36ee24ed45fa98fdcf1e
Rebecca Tay <rebeccatay92@gmail.com>
2017-08-10 23:56:07 +0800

    bollinger 20,2 done. y axes need rescaling

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -1,48 +1,50 @@
-$(document).on('ready page:load', function(event) {
+$(document).on('ready page:load', function (event) {
   console.log('javascript is running...')
-  /*------------------------------------------------*/
+  /* ------------------------------------------------ */
   var currencySym = 'BTC'
   var timeDigit = 60
   var timeFrame = 'minute'
   var bufferPeriods = 50
   var indicator1data = []
   var indicator2data = []
-  /*------------------------------------------------*/
+  /* ------------------------------------------------ */
   // update chart based on selection changes
-  $('button').on('click', function() {
+  $('button').on('click', function () {
     // console.log($(':selected'))
-    dataArr=[]
+    dataArr = []
     currencySym = $(':selected')[0].id
     timeDigit = $(':selected')[1].id
     timeFrame = $(':selected')[1].className
     indicator1 = $(':selected')[2].id
     indicator2 = $(':selected')[3].id
-    //which api to call. min/hr/day
+    indicator3 = $(':selected')[4].id
+    // which api to call. min/hr/day
 
     totalTimePeriods = parseInt(timeDigit) + bufferPeriods - 1
-    var histoQuery =`https://min-api.cryptocompare.com/data/histo${timeFrame}?tsym=USD&fsym=${currencySym}&limit=${totalTimePeriods}`
+    var histoQuery = `https://min-api.cryptocompare.com/data/histo${timeFrame}?tsym=USD&fsym=${currencySym}&limit=${totalTimePeriods}`
 
-    $.get(histoQuery).done(function(x) {
+    $.get(histoQuery).done(function (x) {
       apidata = x.Data
 
       // for prices remove day -50 to 0
       close = JSON.parse(JSON.stringify(apidata)).splice(50)
-      close.forEach(function(e) {
-        e.time =  new Date(e.time * 1000)
+      close.forEach(function (e) {
+        e.time = new Date(e.time * 1000)
         e.value = e.close
       })
-      //always chart the price
+      // always chart the price
       dataArr.push(close)
 
       if (indicator1) {
         var periods = indicator1.substring(3)
         sma = JSON.parse(JSON.stringify(apidata)).splice(0)
         for (i = 50; i < sma.length; i++) {
-          var periodsArr = sma.slice(i-periods,i)
-          var sum = periodsArr.reduce(function(a,b) {
-            return {close: (a.close + b.close)}
-          })
-          var avg = sum.close/periods
+          var periodsArr = sma.slice(i - periods, i)
+          // var sum = periodsArr.reduce(function(a,b) {
+          //   return {close: (a.close + b.close)}
+          // })
+          // var avg = sum.close/periods
+          var avg = averageClose(periodsArr)
           sma[i].value = avg
           sma[i].time = new Date(sma[i].time * 1000)
         }
@@ -54,11 +56,12 @@ $(document).on('ready page:load', function(event) {
         var periods = indicator2.substring(3)
         sma = JSON.parse(JSON.stringify(apidata)).splice(0)
         for (i = 50; i < sma.length; i++) {
-          var periodsArr = sma.slice(i-periods,i)
-          var sum = periodsArr.reduce(function(a,b) {
-            return {close: (a.close + b.close)}
-          })
-          var avg = sum.close/periods
+          var periodsArr = sma.slice(i - periods, i)
+          // var sum = periodsArr.reduce(function(a,b) {
+          //   return {close: (a.close + b.close)}
+          // })
+          // var avg = sum.close/periods
+          var avg = averageClose(periodsArr)
           sma[i].value = avg
           sma[i].time = new Date(sma[i].time * 1000)
         }
@@ -66,21 +69,40 @@ $(document).on('ready page:load', function(event) {
         dataArr.push(indicator2data)
       }
 
+      if (indicator3) {
+        var bollingerLower = JSON.parse(JSON.stringify(apidata)).splice(0)
+        var bollingerUpper = JSON.parse(JSON.stringify(apidata)).splice(0)
+        for (i = 50; i < bollingerLower.length; i++) {
+          var periodsArr = bollingerLower.slice(i - 20, i)
+          var avg = averageClose(periodsArr)
+          var stdDev = standardDeviation(periodsArr)
+
+          bollingerLower[i].value = avg - 2 * stdDev
+          bollingerLower[i].time = new Date(bollingerLower[i].time * 1000)
+
+          bollingerUpper[i].value = avg + 2 * stdDev
+          bollingerUpper[i].time = new Date(bollingerUpper[i].time * 1000)
+        }
+        indicator3upper = bollingerUpper.splice(50)
+        indicator3lower = bollingerLower.splice(50)
+        dataArr.push(indicator3lower, indicator3upper)
+      }
+
       plot()
 
       volume = JSON.parse(JSON.stringify(apidata)).splice(50)
-      volume.forEach(function(e) {
-        e.time =  new Date(e.time * 1000)
+      volume.forEach(function (e) {
+        e.time = new Date(e.time * 1000)
         e.value = e.volumeto
       })
       plotVolume()
     })
   })
 
-  /*------------------------------------------------*/
-  function plot() {
+  /* ------------------------------------------------ */
+  function plot () {
     // finding min.max value for y axis
-    closeSort = close.sort(function(a,b) {
+    closeSort = close.sort(function (a, b) {
       return a.value - b.value
     })
     // console.log(closeSort)
@@ -98,14 +120,14 @@ $(document).on('ready page:load', function(event) {
       y_label: 'USD',
       yax_format: d3.format('2'),
       min_y: closeSort[0].value,
-      //sets axis min
+      // sets axis min
       max_y: closeSort[closeSort.length - 1].value,
       area: false,
       aggregate_rollover: true
     })
   }
 
-  function plotVolume() {
+  function plotVolume () {
     MG.data_graphic({
       // title: 'Historical Price',
       // description: 'Closing',
@@ -122,16 +144,40 @@ $(document).on('ready page:load', function(event) {
       // yax_format: d3.format('2'),
       min_y_from_data: true,
       // min_y: volumeSort[0].value,
-      //sets axis min
+      // sets axis min
       // max_y: volumeSort[volumeSort.length - 1].value,
       aggregate_rollover: true
     })
   }
-  /*------------------------------------------------*/
+
+  function standardDeviation (arrOfObjs) {
+    var avg = averageClose(arrOfObjs)
+    var sqDiff = []
+    arrOfObjs.forEach(function (e) {
+      var diff = e.close - avg
+      sqDiff.push(diff * diff)
+    })
+    var sumSqDiff = sqDiff.reduce(function (a, b) {
+      return a + b
+    })
+    var avgSqDiff = sumSqDiff / 20
+    var stdDev = Math.sqrt(avgSqDiff)
+    return stdDev
+  }
+
+// finding avg in array of objects (closing price)
+  function averageClose (arrOfObjs) {
+    var sum = arrOfObjs.reduce(function (a, b) {
+      return {close: (a.close + b.close)}
+    })
+    var avg = sum.close / arrOfObjs.length
+    return avg
+  }
+  /* ------------------------------------------------ */
   var currentPriceApi = `https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,XRP,LTC,DASH&tsyms=USD`
 
-  function getCurrentPrice(){
-    $.get(currentPriceApi).done(function(data){
+  function getCurrentPrice () {
+    $.get(currentPriceApi).done(function (data) {
       // console.log(data)
       for (var key in data) {
         // console.log(key) //coin symbol
@@ -142,5 +188,5 @@ $(document).on('ready page:load', function(event) {
   } // close fn
   getCurrentPrice()
   setInterval(getCurrentPrice, 10000)
-  /*------------------------------------------------*/
-}) //close doc.ready
+  /* ------------------------------------------------ */
+}) // close doc.ready
