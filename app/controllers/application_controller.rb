75c0f9e6a170d353d4130b97001462d8234d03fc
kepler62f <blue.kepler62f@gmail.com>
2017-08-10 23:32:11 +0800

    basic routing and gating of pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,15 @@ protect_from_forgery with: :exception
 
 before_action :configure_permitted_parameters, if: :devise_controller?
 before_action :authenticate_user!
+
 protected
 
 def configure_permitted_parameters
   devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :credits_remaining])
 end
 
-
+def application
+  @user_name = current_user.name
+end
 
 end
