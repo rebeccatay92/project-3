9c49fcdc099cdac13f553d4e2c34c4720ac65ea3
Azmeer Siddiq <reemza77@gmail.com>
2017-08-14 13:21:57 +0800

    transaction works

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -9,19 +9,19 @@ def create
   # new_transaction.units = 2
   # new_transaction.amount_unit = params[:transaction][:amount_unit]
 
-# new_transaction = Transaction.new
-#   new_transaction.user_id = current_user[:id]
-#   new_transaction.currency_id = params[:transaction][:currency_id]
-#   new_transaction.txn_type = 1
-#   new_transaction.units = params[:transaction][:units]
-#   new_transaction.amount_unit = params[:transaction][:amount_unit]
-#
-#   new_transaction.save
+new_transaction = Transaction.new
+  new_transaction.user_id = current_user[:id]
+  new_transaction.currency_id = params[:transaction][:currency_id]
+  new_transaction.txn_type = 1
+  new_transaction.units = params[:transaction][:units]
+  new_transaction.amount_unit = params[:transaction][:amount_unit]
 
-  a = Transaction.new
-  a.user_id = 1
-  a.save
-  puts a.inspect
+  new_transaction.save!
+  #
+  # a = Transaction.new
+  # a.user_id = 1
+  # a.save!
+  # puts a.inspect
 #
 #   puts new_transaction.inspect
 
