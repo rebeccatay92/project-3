5f1a9d0e9c2fdad5d011ea1f061920f8e5ca7777
Rebecca Tay <rebeccatay92@gmail.com>
2017-08-15 10:00:54 +0800

    added seed data for currencies

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Currency.create(name:"Bitcoin", currency_symbol:"BTC")
+Currency.create(name:"Ethereum", currency_symbol:"ETH")
+Currency.create(name:"Ripple", currency_symbol:"XRP")
+Currency.create(name:"Litecoin", currency_symbol:"LTC")
+Currency.create(name:"Dash", currency_symbol:"DASH")
