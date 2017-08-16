90737d2eab89ee376abf7ea7f57a654380c8cfc5
Azmeer Siddiq <reemza77@gmail.com>
2017-08-16 16:58:20 +0800

    dom done

--- app/assets/javascripts/portfolios.js
+++ app/assets/javascripts/portfolios.js
@@ -3,6 +3,32 @@ $(document).on('turbolinks:load', function (event) {
 
   $('.portfolios.index').ready(function () {
 
+$('.sell_currency').on('keyup', '.text_field' , function(e){
+// console.log(e.target.value);
+var units = e.target.value
+// console.log($(this).parent().parent().children('.sell_price')[0].innerText);
+var priceNode = $(this).parent().parent().children('.buy_price')[0]
+var priceString = priceNode.innerText
+var price = priceString.split('$')[1]
+console.log('price is ' + price + 'and there are ' + units + 'units')
+var transaction_node = $(this).parent().children('#cost')
+transaction_node.text('Transaction value:  US$'+(price*units).toFixed(2))
+
+})
+
+$('.buy_currency .text_field').on('keyup', function(e){
+console.log(e.target.value);
+
+var units = e.target.value
+// console.log($(this).parent().parent().children('.buy_price')[0].innerText);
+var priceNode = $(this).parent().parent().children('.buy_price')[0]
+var priceString = priceNode.innerText
+var price = priceString.split('$')[1]
+console.log('price is ' + price + 'and there are ' + units + 'units')
+var transaction_node = $(this).parent().children('#cost')
+transaction_node.text('Transaction value:  US$'+(price*units).toFixed(2))
+
+})
   //   function refresh() {
   //     location.reload()
   //   }
