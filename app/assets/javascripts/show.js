dd265606f29ffed9d5b32a1b8f9a3081ba5c008a
kepler62f <blue.kepler62f@gmail.com>
2017-08-16 17:22:26 +0800

    fixed dataTables issues: reinit, back button issue

--- app/assets/javascripts/show.js
+++ app/assets/javascripts/show.js
@@ -4,21 +4,39 @@ $(document).on('turbolinks:load', function (event) {
 
   $('.social_trading.show').ready(function () {
 
-    $('#portfolioTable').DataTable({
-       searching: false,
-       ordering: false,
-       select: false,
-       paging: false,  // **** TURN ON IF COINS NO LONGER LIMITED TO 5
-       // pagingType: "simple"
-    })
 
-    $('#transactionsTable').DataTable({
-        searching: false,
-        ordering: false,
-        select: false,
-        paging: true,
-        pagingType: "simple"
-    })    
+    // to clear entries selection dropdown/info, next/prev page selector
+    // otherwise will new ones will be created if user clicks browser 'back' button
+    if ($('.dataTables_length')) {
+      $('.dataTables_length').remove()
+    }
+    if ($('.dataTables_info')) {
+      $('.dataTables_info').remove()
+    }
+    if ($('.dataTables_paginate.paging_simple')) {
+      $('.dataTables_paginate.paging_simple').remove()
+    }
+
+
+    if (!($.fn.dataTable.isDataTable('#portfolioTable'))) {
+      $('#portfolioTable').DataTable({
+         searching: false,
+         ordering: false,
+         select: false,
+         paging: false,  // **** TURN ON IF COINS NO LONGER LIMITED TO 5
+         // pagingType: "simple"
+      })
+    }
+
+    if (!($.fn.dataTable.isDataTable('#transactionsTable'))) {
+      $('#transactionsTable').DataTable({
+          searching: false,
+          ordering: false,
+          select: false,
+          paging: true,
+          pagingType: "simple"
+      })
+    }    
 
     // take ruby server time and convert to user's local time
     var d = $('.dtn').data('datetimenow') 
